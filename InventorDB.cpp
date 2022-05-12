9aa0fcd9a990fee85fb1b81d662896282e218b99
SHERAPVERSLUYS <>
2022-05-12 16:14:06 +0000

    Done

--- InventorDB.cpp
+++ InventorDB.cpp
@@ -1,4 +1,5 @@
 #include "InventorDB.h"
+//#include "Inventor.cpp"
 #include "ReadUtils.h"
 #include <iostream>
 #include <fstream>
@@ -9,6 +10,8 @@ using namespace std;
 Set head and tail to nullptr
 */
 InventorDB::InventorDB(){
+  head = nullptr;
+  tail = nullptr;
 }
 
 InventorDB::~InventorDB() {
@@ -17,30 +20,80 @@ InventorDB::~InventorDB() {
 
 // Delete all the current nodes and set head & tail to nullptr
 void InventorDB::deleteAllNodes() {
+  while(head != nullptr) {
+  	Node *next = head->next;
+  	delete head;
+  	head = next;
+}
+tail = nullptr;
 }
 
 // Append a new entry at the tail of the list
 void InventorDB::append(Inventor inventor) {
+Node *nodePtr = new Node(inventor);
+
+  if(head == nullptr){
+  tail = nodePtr;
 }
+  else{
+    nodePtr -> next = head;
+  }
+head = nodePtr;
+  }
 
 // Traverse the list printing all the data in the nodes
 void InventorDB::print() {
+  Node *curr = head;
+  while(curr != nullptr){
+    curr->data.print();
+    curr = curr -> next;
+  }
 }
 
 // Insert a new entry at the head of the list
 void InventorDB::insertInFront(Inventor inventor){
+Node *nodePtr = new Node(inventor);
+  
+  if(head == nullptr){
+  tail = nodePtr;
+}
+  else{
+    nodePtr -> next = head;
+  }
+head = nodePtr;
 }
 
 // If there is an entry at the head of the list, remove it.
 void InventorDB::removeFront() {
+  
+  if(head != nullptr){
+  Node *next = head -> next;
+  delete head;
+  head = next;
+}
+if(head == nullptr){
+  tail = nullptr;
+}
 }
 
 
 // Traverse the list and if a call to isAlive on the data
 // in the node returns true, print that data.
-void InventorDB::printAlive() {
+void InventorDB::printAlive(){
+  
+Node *curr = head;
+  while(curr != nullptr) {
+      curr -> data.isAlive();
+    if (curr -> data.isAlive() == true){
+         Node *curr = head;
+  while(curr != nullptr){
+    curr->data.print();
+    curr = curr -> next;
+  }
+  }
 }
-
+	curr = curr->next;
+  }
 
 
 /**
